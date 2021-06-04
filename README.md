a3b5f0ad74cd5013082dc62e8a6b7c60a86a90e9
dongjiahua <cnjiahuadong@gmail.com>
2021-06-04 12:55:07 +0800

    generate executable file and a shell version compiler

--- README.md
+++ README.md
@@ -4,11 +4,54 @@
 
   test code files in Pascal
 
-* src
+### Setup
 
-  main.py : implement lexical analysis, syntax analysis, intermediate representation generation and target code generation
+This project has following  requirements:
 
-  lexer.py : lexical analysis
-  yacc.py : syntax analysis
-  ast.py : AST nodes and the corresponding irgen() methods
-  helper.py : implement symbol table and other helper methods
+* python>=3.6
+
+* llvmlite
+* Clang compiler
+* ply
+
+To ensure your work space correct, we suggest to use virtual enviroment. 
+
+if you don't have a python3 environment:
+
+```
+conda create -n pas_compiler python=3.8
+conda activate pas_compiler
+```
+
+Then install the required packages:
+
+```
+pip install -r requirements.txt
+```
+
+you also have to install clang. this process will be omittd here.
+
+### Run Pas_compiler
+
+```
+python src/main.py <input_file> 
+		<-o output_file>    # output_file name, "a.out" as default
+  	<-O optim_level>    # optimization_level, 2 as default
+  	<-ir ir_file>       # ir file name, program won't output ir file as default
+  	<-asm asm_file>     # asm file name, won't output machine code as default
+```
+
+to take a quick example, you can use:
+
+```
+python src/main.py pascal/NestedFunctionTest.pas
+```
+
+### src
+
+main.py : implement lexical analysis, syntax analysis, intermediate representation generation and target code generation
+
+lexer.py : lexical analysis
+yacc.py : syntax analysis
+ast.py : AST nodes and the corresponding irgen() methods
+helper.py : implement symbol table and other helper methods
--- src/main.py
+++ src/main.py
@@ -1,36 +1,55 @@
 import llvmlite.binding as llvm
 from yacc import Parser
+import tempfile
+import subprocess
+import os
+import argparse
+
+_rtlib = os.path.join(os.path.dirname(__file__), 'lib.c')
+
+
+def optimize(ir, args):
+    llvm_module = llvm.parse_assembly(ir)
+    pmb = llvm.create_pass_manager_builder()
+    pmb.opt_level = args.optimization_level
+    pm = llvm.create_module_pass_manager()
+    pmb.populate(pm)
+    pm.run(llvm_module)
+    return llvm_module
+
 
 if __name__ == '__main__':
-    
-    parser = Parser()   # syntax analysis
+    args = argparse.ArgumentParser()
+    args.add_argument("input_file", help='specify the input file')
+    args.add_argument("-O", "--optimization_level", type=int, help="specify the optimization level", default=2)
+    args.add_argument("-ir", "--ir_file", help="generate ir code")
+    args.add_argument("-asm", "--asm_file", help="generate machine code")
+    args.add_argument("-o", "--output_file", help="output file name", default="a.out")
+    args = args.parse_args()
 
-    fin = open("/Users/xy/Compiler/pascal/NestedFunctionTest.pas")
-    root = parser.parse(fin.read().lower())
-    fin.close()
+    parser = Parser()  # syntax analysis
 
-    root.irgen()    # intermediate representation generation
+    with open(args.input_file) as fin:
+        root = parser.parse(fin.read().lower())
+    root.irgen()  # intermediate representation generation
 
-    print('=== LLVM IR')
-    print(root.module)  # print ir
-
+    if args.ir_file:
+        with open(args.ir_file,"w")as ir:
+            ir.write(str(root.module))
     # Convert textual LLVM IR into in-memory representation.
-    llvm_module = llvm.parse_assembly(str(root.module))
-    tm = llvm.Target.from_default_triple().create_target_machine()
 
+    llvm_module = optimize(str(root.module), args)
+    # Compile the module to machine code using MCJIT
+    output_file = "-o " + args.output_file.strip()
+    with tempfile.NamedTemporaryFile(suffix='.ll') as f:
+        f.write(str(llvm_module).encode('utf-8'))
+        f.flush()
+        subprocess.check_output(["clang", output_file,'-DNEED_MAIN', f.name, _rtlib])
+
+    tm = llvm.Target.from_default_triple().create_target_machine()
     # Compile the module to machine code using MCJIT
     with llvm.create_mcjit_compiler(llvm_module, tm) as ee:
         ee.finalize_object()
-        print('=== Assembly')
-        print(tm.emit_assembly(llvm_module))    # print assembly
-
-        # cfptr = ee.get_function_address("cal")
-
-        # from ctypes import CFUNCTYPE, c_int
-        # # To convert an address to an actual callable thing we have to use
-        # # CFUNCTYPE, and specify the arguments & return type.
-        # cfunc = CFUNCTYPE(c_int, c_int)(cfptr)
-
-        # # Now 'cfunc' is an actual callable we can invoke
-        # res = cfunc(15, 5)
-        # print('The result is', res)
+        if args.asm_file:
+            with open(args.asm_file, 'w') as f:
+                f.write(tm.emit_assembly(llvm_module))
